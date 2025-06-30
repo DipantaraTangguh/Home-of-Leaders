d3b265434a45e847081cea45c287a5f3b9ab103c
Akbarnama Tangguh Dipantara <tangguhdipantara@outlook.co.id>
2025-06-30 09:04:34 +0700

    refactor implement tw config

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,28 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import tailwindcss from "@tailwindcss/vite";
+// import defaultTheme from "tailwindcss/defaultTheme";
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   content: ["./index.html", "./src/**/*.{js,jsx}"],
+//   theme: {
+//     extend: {
+//       fontFamily: {
+//         sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+//       },
+//     },
+//   },
+//   plugins: [react(), tailwindcss()],
+//   base: "/",
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import defaultTheme from "tailwindcss/defaultTheme";
 
 // https://vite.dev/config/
 export default defineConfig({
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Poppins", ...defaultTheme.fontFamily.sans],
-      },
-    },
-  },
   plugins: [react(), tailwindcss()],
   base: "/",
 });
